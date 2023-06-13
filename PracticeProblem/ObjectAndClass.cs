3e8f26ea421be4a0f4add9844245095cde89b77a
rishiraj007 <rishiraj@mounttalent.com>
2023-06-13 19:19:50 +0530

    [Prabhu]Add: class and object with example

---
+++ PracticeProblem/ObjectAndClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class ObjectAndClass
+    {
+        private double length;
+        private double width;
+
+        public ObjectAndClass(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public double CalculateArea()
+        {
+            return length * width;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (length + width);
+        }
+    }
+}
--- PracticeProblem/Program.cs
+++ PracticeProblem/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace PracticeProblem
+﻿using System.Drawing;
+
+namespace PracticeProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("OOPs Concept - Practice Problem !");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nSelect option \n1.Class and Object \n2.Exit");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:
+                        ObjectAndClass objectAndClass = new ObjectAndClass(5.0, 3.0);
+                        double area = objectAndClass.CalculateArea();
+                        double perimeter = objectAndClass.CalculatePerimeter();
+                        Console.WriteLine("Area: " + area);
+                        Console.WriteLine("Perimeter: " + perimeter);
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
